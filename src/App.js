fdbb9cf62ceb352573d4dace4b0be2a7ef790521
SymonMuchemi <simonkenya846@gmail.com>
2023-10-04 02:59:59 +0300

    Defineing state variable using useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -6,34 +8,43 @@ const messages = [
 
 
 function App() {
-	let step = 1;
+	const [step, setStep] = useState(1);
 
-  return (
-    <div className="steps">
-		<div className="numbers">
-			<div className="active">1</div>
-			<div>2</div>
-			<div>3</div>
-		</div>
-		
-		<p className="message">Step {step}: {messages[step - 1]} </p>
+	function handlePrevious(){
 
-		<div className="buttons">
-			<button
-				style={{
-					backgroundColor: '#7950f2',
-					color: "#FFF"
-				}} 
-				>Previous</button>
-			<button
-				style={{
-					backgroundColor: '#7950f2',
-					color: "#FFF"
-				}}
-			>Next</button>
+	}
+
+	function handleNext(){
+
+	}
+
+	return (
+		<div className="steps">
+			<div className="numbers">
+				<div className={step >= 1 && 'active'}>1</div>
+				<div className={step >= 2 && 'active'}>2</div>
+				<div className={step >= 3 && 'active'}>3</div>
+			</div>
+			
+			<p className="message">Step {step}: {messages[step - 1]} </p>
+
+			<div className="buttons">
+				<button
+					style={{
+						backgroundColor: '#7950f2',
+						color: "#FFF"
+					}} 
+					>Previous</button>
+				<button
+					style={{
+						backgroundColor: '#7950f2',
+						color: "#FFF"
+					}}
+					onClick={() => (step += 1)}
+				>Next</button>
+			</div>
 		</div>
-    </div>
-    )
+	)
 }
 
 export default App;
