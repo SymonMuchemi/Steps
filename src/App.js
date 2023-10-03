2e50f98aadefc8fff9e0f515a6b25ef0a9453079
SymonMuchemi <simonkenya846@gmail.com>
2023-10-04 02:21:59 +0300

    Access message dynamically

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 
 function App() {
-  
+	let step = 1;
+
+  return (
+    <div className="steps">
+		<div className="numbers">
+			<div className="active">1</div>
+			<div>2</div>
+			<div>3</div>
+		</div>
+		
+		<p className="message">Step {step}: {messages[step - 1]} </p>
+
+		<div className="buttons">
+			<button
+				style={{
+					backgroundColor: '#7950f2',
+					color: "#FFF"
+				}} 
+				>Previous</button>
+			<button
+				style={{
+					backgroundColor: '#7950f2',
+					color: "#FFF"
+				}}
+			>Next</button>
+		</div>
+    </div>
+    )
 }
 
 export default App;
