10b5eaac6e0dd91d4db2581d5486b109dde4fe98
SymonMuchemi <simonkenya846@gmail.com>
2023-10-04 03:39:14 +0300

    created step component

---
+++ src/steps.js
@@ -0,0 +1,31 @@
+export default function Steps({step, handlePrevious, handleNext, messages}){
+
+    return (
+        <div className="steps">
+			<div className="numbers">
+				<div className={step >= 1 && 'active'}>1</div>
+				<div className={step >= 2 && 'active'}>2</div>
+				<div className={step >= 3 && 'active'}>3</div>
+			</div>
+			
+			<p className="message">Step {step}: {messages[step - 1]} </p>
+
+			<div className="buttons">
+				<button
+					style={{
+						backgroundColor: '#7950f2',
+						color: "#FFF"
+					}}
+					onClick={handlePrevious}
+					>Previous</button>
+				<button
+					style={{
+						backgroundColor: '#7950f2',
+						color: "#FFF"
+					}}
+					onClick={handleNext}
+				>Next</button>
+			</div>
+		</div>
+    );
+}
